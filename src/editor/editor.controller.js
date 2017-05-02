f87e8c8fb44776ac89b6b5fbee4fd850d38d65fe
Antoine Renault <antoine.renault@orson.io>
2017-05-02 10:37:01 +0200

    Fix issue with list no displaying, fix missing not being present

--- src/editor/editor.controller.js
+++ src/editor/editor.controller.js
@@ -24,46 +24,43 @@ module.exports = function ($rootScope, $scope, GenericDatas, AlertManager) {
    * @param {Object} item
    * @returns
    */
-  // $scope.markMissingTranslations = function (item) {
-  //   var source = $scope.initialLang
-  //   var target = $scope.totranslateLang
-  //   // Right-sand side
-  //   var rhs = item[source]
-  //   // Left-hand side
-  //   var lhs = item[target]
-  //   // If both are present, nothing is missing
-  //   if (isPresentString(rhs) && isPresentString(lhs)) {
-  //     delete item.missing
-  //   } else {
-  //     // Ensure that item has a `missing` property
-  //     item.missing = item.missing || {}
-  //     if (isEmptyString(source)) {
-  //       item.missing[source] = true
-  //     } else if (isEmptyString(target)) {
-  //       item.missing[target] = true
-  //     }
-  //   }
-  //   return item
-  // }
+  $scope.markMissingTranslations = function (item) {
+    console.log(item)
+    if (
+      (!item.lhs || item.lhs.length === 0) ||
+      (!item.rhs || item.rhs.length === 0)
+    ) {
+      item['missing'] = true
+    } else {
+      delete item['missing']
+    }
+    return item
+  }
 
   $scope.$on('filereaded', function (event, arg, filename) {
     var state = arg.state
     $scope[state] = arg.datas
     if ($scope.initial && $scope.totranslate) {
-      var english = $scope.totranslate.en
-      var french = $scope.initial.fr
+      var rhs = $scope.totranslate.en
+      var lhs = $scope.initial.fr
+    } else {
+      return false
     }
-    var objectA = {
-      language: 'fr',
-      translations: flattenObject(french)
+    var objectLhs = {
+      language: 'lhs',
+      translations: flattenObject(lhs)
     }
-    var objectB = {
-      language: 'en',
-      translations: flattenObject(english)
+    var objectRhs = {
+      language: 'rhs',
+      translations: flattenObject(rhs)
     }
-    $scope.initialLang = 'fr'
-    $scope.totranslateLang = 'en'
-    $scope.metadata = mergeObjects(objectA, objectB)
+    $scope.initialLang = 'lhs'
+    $scope.totranslateLang = 'rhs'
+    var mergedData = mergeObjects(objectLhs, objectRhs)
+    var markedData = markedMissing(mergedData)
+    $scope.metadata = markedData
+
+    console.log('Metadata', $scope.metadata)
     // console.log(metadata)
     // $scope[state + 'Lang'] = $scope.getLanguage($scope[state])
     // // It's only when we have both files that we start processing the keys
@@ -93,8 +90,7 @@ module.exports = function ($rootScope, $scope, GenericDatas, AlertManager) {
     //   )
   })
 
-
-  // Takes a nested object and produces a result objects where all the nested paths have 
+  // Takes a nested object and produces a result objects where all the nested paths have
   // been flattened (segments are separated by /)
   var flattenObject = function (obj, path, flattened) {
     path = path || []
@@ -109,9 +105,20 @@ module.exports = function ($rootScope, $scope, GenericDatas, AlertManager) {
     return flattened
   }
 
+  var markedMissing = function (data) {
+    console.log('Marked missing data', data)
+    Object.keys(data).forEach(function (key) {
+      var current = data[key]
+      if (current.lhs.length === 0 || current.rhs.length === 0) {
+        current['missing'] = true
+      }
+    })
+    return data
+  }
+
   // Merges two objects in one object that Angular can use to display the translation table
   // The keys that exist on the left hand side object are looked up in the right hand side object
-  // If a value exists there, we stuff it in the common path and then we delete it from the right hand side 
+  // If a value exists there, we stuff it in the common path and then we delete it from the right hand side
   // object (to speed up operations for the second phase)
   // If it doesn't exist, we add it to the common path with a '' value
   // In the second phase we do the same by using the right hand side as the base (i.e. keys that exist in the rhs, but not in the lhs)
@@ -133,77 +140,6 @@ module.exports = function ($rootScope, $scope, GenericDatas, AlertManager) {
     return result
   }
 
-  // var convertToArray = function (translations, lang) {
-  //   function convert(translations, oldPath, newPath, lang, data) {
-  //     var path = oldPath + '/' + newPath
-  //     if (newPath === '' && oldPath === '') {
-  //       path = ''
-  //     }
-  //     for (var property in translations) {
-  //       if (translations.hasOwnProperty(property)) {
-  //         if (typeof translations[property] === 'object') {
-  //           convert(translations[property], path, property, lang, data)
-  //         } else {
-  //           data.push({
-  //             key: property,
-  //             lang: translations[property],
-  //             path: path
-  //           })
-  //         }
-  //       }
-  //     }
-  //     return data
-  //   }
-  //   return convert(translations, '', '', lang, [])
-  // }
-
-  // var extractDiffKeys = function (a, b, lang, mode) {
-  //   for (var i = 0; i < a.length; i++) {
-  //     var isPresent = false
-
-  //     for (var j = 0; j < b.length; j++) {
-  //       if (b[j].key === a[i].key) {
-  //         if (angular.isDefined(mode)) {
-  //         } else {
-  //           a[i][lang] = b[j][lang]
-  //         }
-  //         isPresent = true
-  //       }
-  //     }
-
-  //     if (!isPresent) {
-  //       if (angular.isDefined(mode)) {
-  //         b[j] = a[i]
-  //         b[j][lang] = ''
-  //         b[j]['missing'] = {}
-  //         b[j]['missing'][lang] = true
-  //       } else {
-  //         a[i][lang] = ''
-  //         a[i]['missing'] = {}
-  //         a[i]['missing'][lang] = true
-  //       }
-  //     }
-  //   }
-  // }
-
-
-  // var addPathToObject = function (object, path, key, value) {
-  //   var prop = path.shift()
-  //   // We're at the end of the path
-  //   if (typeof prop === 'undefined') {
-  //     object[key] = value
-  //     return object
-  //   } else {
-  //     // If the path segment doesn't exist at this level of the object
-  //     // create it with an empty object and keep on descending
-  //     if (!object.hasOwnProperty(prop)) {
-  //       object[prop] = {}
-  //     }
-  //     // Default case, the path segment exists, so we keep on descending
-  //     addPathToObject(object[prop], path, key, value)
-  //   }
-  // }
-
   // var checkEmptyValue = function (datas, lang) {
   //   for (var i = 0; i < datas.length; i++) {
   //     if (datas[i][lang] === '') {
@@ -213,11 +149,11 @@ module.exports = function ($rootScope, $scope, GenericDatas, AlertManager) {
   //   return false
   // }
 
-
   // We need to export both versions (lhs and rhs) since values might have been added on both sides
   $scope.export = function (datas, lang) {
-    debugger;
-    //console.log($scope.filename)
+    console.log('Datas', datas)
+    console.log('Lang', lang)
+    // console.log($scope.filename)
     //   if (checkEmptyValue(datas, lang)) {
     //     AlertManager.add({
     //       type: 'danger',
