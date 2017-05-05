ae713ef78b458aac4dece558a056343f9b200a3e
Antoine Renault <antoine.renault@orson.io>
2017-05-05 11:25:11 +0200

    Global cleaning

--- src/app.config.js
+++
@@ -1,2 +0,0 @@
-'use strict'
-module.exports = /*@ngInject*/ function ($httpProvider, AppConfig, $translateProvider) {};
--- src/header/header.controller.js
+++
@@ -1,6 +0,0 @@
-/*global module */
-
-module.exports = function() {
-    'ngInject';
-    'use strict';
-};
