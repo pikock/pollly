97dfee0099b65af5f4b52a42f30160ad54720ce3
Antoine Renault <antoine.renault@orson.io>
2017-07-28 09:06:45 +0200

    Remove debugger

--- src/editor/controllers/exportController.js
+++ src/editor/controllers/exportController.js
@@ -2,6 +2,5 @@ module.exports = window.angular
   .module('exportController', ['ui.bootstrap'])
   .controller('exportController', function(variable, arg) {
     'ngInject'
-    debugger
     this.modalText = 'Modal Text'
   }).name
